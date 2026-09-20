3e771a0b340efa70176204259ce09fd2361ae4a6
shathazyadeh <shatha.zyadeh@gmail.com>
2026-09-20 13:38:16 +0300

    Add BaseTest setup and teardown methods for browser initialization

--- src/base/BaseTest.java
+++ src/base/BaseTest.java
@@ -1,7 +1,31 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class BaseTest {
 
-		
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://certwcs.grandinroad.com/?aka_bypass=5C73514EE7A609054D81DE61DD9CA3D6\r\n");
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		if(driver != null) {
+			driver.quit();
+		}
+	}
+	@Test
+	public void sampleTest() {
+	    System.out.println("TestNG");
+	}
 }
 
