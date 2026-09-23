d2a034e5f7d45412fba3fb66468361067e9e7fd5
shathazyadeh <shatha.zyadeh@gmail.com>
2026-09-24 01:09:27 +0300

    Update ProductDetailsPage , add cart verification and close modal methods and add CartPage class

--- src/pages/ProductDetailsPage.java
+++ src/pages/ProductDetailsPage.java
@@ -15,6 +15,10 @@ public class ProductDetailsPage {
     By addToCartLocator = By.cssSelector("button[data-analytics-name='add_to_cart']");
     By colorSwatchLocator = By.cssSelector("button.c-universal-options__option-swatch"); 
     
+    By continueShoppingLocator = By.cssSelector("button.c-button.c-custom-sheet__close-button, button.c-custom-sheet__close-button");
+    By miniCartModalLocator = By.cssSelector("div.c-custom-sheet__content");
+    By successMessageLocator = By.cssSelector(".c-items-added-to-cart"); // add
+    
     
     public ProductDetailsPage(WebDriver driver) {
         this.driver = driver;
@@ -77,5 +81,54 @@ public class ProductDetailsPage {
             return false;
         }
     }
+    
+    
+    
+    
+    public void clickAddToCart() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement addButton = wait.until(ExpectedConditions.elementToBeClickable(addToCartLocator));
+        addButton.click();
+        System.out.println("Clicked 'Add to Cart' successfully.");
+    }
+    
+    
+    
+    
+    public boolean isProductAddedSuccessfully() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(successMessageLocator));
+            boolean isDisplayed = message.isDisplayed();
+            if (isDisplayed) {
+                System.out.println(" Product successfully added to cart ' item added to cart' message is displayed'");
+            }
+            return isDisplayed;
+        } catch (Exception e) {
+            System.out.println("Success message did not appear.");
+            return false;
+        }
+    }
+    
+    
+    
+    public void closeMiniCartIfDisplayed() {
+        try {
+            WebDriverWait shortWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            WebElement closeButton = shortWait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector("button.c-custom-sheet__close-button, .c-icon.c-custom-sheet__close-icon")
+            ));
+            closeButton.click();
+            System.out.println("Closed mini-cart modal successfully.");
+            
+            // انتظار قصير لاختفاء النافذة
+            shortWait.until(ExpectedConditions.invisibilityOf(closeButton));
+        } catch (Exception e) {
+            System.out.println("Mini-cart modal did not appear or already closed.");
+        }
+    }
+    
+    
+    
 
     }
---
+++ src/testsCases/CartTest.java
@@ -0,0 +1,5 @@
+package testsCases;
+
+public class CartTest {
+
+}
